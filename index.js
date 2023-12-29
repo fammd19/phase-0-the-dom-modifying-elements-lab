98ea16a273ad490d7e93b9fce94d57300de798a4
Fiona Mellink <fimellink@gmail.com>
2023-12-30 10:36:50 +1100

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const main = document.querySelector('main');
+main.remove ();
+
+const newHeader = document.createElement('h1');
+//document.body.append(newHeader);
+newHeader.id = "victory";
+newHeader.textContent = "Fi is the champion";
+
